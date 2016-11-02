bcbfe1ed9f1d1083398f6d94c1baee48d1445e2e
ckcks12 <wyoducc@gmail.com>
2016-11-03 01:03:50 +0900

    *add Video Writer

--- client/main.cpp
+++ client/main.cpp
@@ -17,6 +17,7 @@ int main() {
     const int HEIGHT = 360;
 
     int input;
+    deque<Mat> que;
 
     curl_global_init(CURL_GLOBAL_ALL);
 
@@ -68,15 +69,41 @@ int main() {
         cam1.open(2);
         cam1.set(CV_CAP_PROP_FRAME_WIDTH, WIDTH);
         cam1.set(CV_CAP_PROP_FRAME_HEIGHT, HEIGHT);
-        cam1.set(CV_CAP_PROP_FOCUS, 1);
-        cam1.set(CV_CAP_PROP_AUTO_EXPOSURE, false);
-        cam1.set(CV_CAP_PROP_AUTOGRAB, false);
+//        cam1.set(CV_CAP_PROP_FOCUS, 1);
+//        cam1.set(CV_CAP_PROP_AUTO_EXPOSURE, false);
+//        cam1.set(CV_CAP_PROP_AUTOGRAB, false);
+        cam1.set(CV_CAP_PROP_FPS, 25);
 
-        while( waitKey(33) != 32 )
+        while( waitKey(30) != 32 )
         {
             cam1 >> mat;
             imshow("background select", mat);
         }
+
+        while( waitKey(1) != 32 )
+        {
+            cam1 >> mat;
+            que.push_back(mat.clone());
+            imshow("background select", mat);
+        }
+
+        VideoWriter writer;
+//        int ex = (int)cam1.get(CV_CAP_PROP_FOURCC);
+//        ex = CV_FOURCC('M', 'P', '4', '2');
+//        ex = CV_FOURCC('M', 'J', 'P', 'G');
+//        ex = CV_FOURCC('X', 'V', 'I', 'D');
+//        ex = CV_FOURCC('D', 'I', 'V', 'X');
+        int ex = CV_FOURCC('m', 'p', '4', 'v'); // for OSX
+        writer.open("/Users/lec/Desktop/SafeDesk/1.avi", ex, 15.0, Size(WIDTH, HEIGHT));
+        while( ! que.empty() )
+        {
+            mat = que.front();
+            writer << mat;
+            que.pop_front();
+        }
+        writer.release();
+
+        return 0;
 
         while( waitKey(33) != 32 )
         {
@@ -115,6 +142,7 @@ int main() {
 
         // delete noise
         bd.deleteNoise(mat);
+        imshow("delete noise", mat);
 
         // retrieve contours
         vector<vector<Point>> contours;
@@ -128,9 +156,9 @@ int main() {
 
         // get star skeleton features
         vector<Point> features = skeleton.getFeatures(contour, dist);
-        assert(features.size() > 5);
+        assert(features.size() > 0);
 
-        for( int i=0; i<5; i++ )
+        for( int i=0; i<features.size(); i++ )
         {
             circle(original_mat, features[i], 3, Scalar(0, 255, 0), 3);
         }
